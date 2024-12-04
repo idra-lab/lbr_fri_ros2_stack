be0f0a3bf677608ca36ff17634802236d52ee004
mhubii <m.huber_1994@hotmail.de>
2024-12-04 17:49:27 +0000

    adds wrench comand (https://github.com/lbr-stack/lbr_fri_ros2_stack/pull/233#discussion_r1869192458)

--- lbr_fri_ros2/src/interfaces/wrench_command.cpp
+++ lbr_fri_ros2/src/interfaces/wrench_command.cpp
@@ -33,6 +33,7 @@ void WrenchCommandInterface::buffered_command_to_fri(fri_command_t_ref command,
     joint_position_filter_.initialize(state.sample_time);
   }
   joint_position_filter_.compute(command_target_.joint_position, command_.joint_position);
+  command_.wrench = command_target_.wrench;
 
   // validate
   if (!command_guard_->is_valid_command(command_, state)) {
